c39d2fe3f261d8102d19f4db89acce9a4aa84b90
adofsauron <adofsauron@163.com>
2022-05-30 11:04:30 +0800

    fix main

--- install-base.sh
+++ install-base.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+yum install -y make cmake gcc gcc-c++ 
+
 yum install -y readline-devel
+
+yum install -y bison flex zlib zlib-devel
 
 # sudo apt install -y libreadline-dev
 # sudo apt install -y zlib1g-dev
