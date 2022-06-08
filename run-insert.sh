c07f7f65a51eb3ee48e08c5cb91c2cbcc0582dae
adofsauron <adofsauron@163.com>
2022-06-08 18:56:34 +0800

    fix main

--- run-insert.sh
+++ run-insert.sh
@@ -2,7 +2,7 @@
 
 psql -U kevin -d test -h localhost -p 5432 -c "select count(1) from company;"
 
-for ((i=1; i<=1000; i++))
+for ((i=1; i<=100000; i++))
 do
 
     SQL="INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY,JOIN_DATE) VALUES ($i, 'Paul_$i', 32, 'California_$i', 20000.00,'2001-07-13');"
