5b9391341d8afd9d92af507ea43524e39fd2736a
adofsauron <adofsauron@163.com>
2022-06-26 08:22:56 +0800

    fix main

--- run-postgres.sh
+++ run-postgres.sh
@@ -5,15 +5,20 @@ HERE=`pwd`
 # adduser kevin
 # su kevin
 
+mkdir -p /var/run/postgresql
 chown -R kevin:kevin /var/run/postgresql
 
 export PGDATA=/home/kevin/postgres/data
+
 pg_ctl  stop
 
 sleep 3s
 
 # rm -rf /home/kevin/postgres/data
+
 mkdir -p  /home/kevin/postgres/data
+
+chown -R kevin:kevin /home/kevin/postgres/data
 
 export PGDATA=/home/kevin/postgres/data
 
